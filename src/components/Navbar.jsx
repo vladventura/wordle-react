764a1fdbcc72ec21f6442652e743abed92e2bc73
Vladimir Ventura <vladventura@outlook.es>
2022-04-20 11:11:51 -0400

    Finished theming. Will remove downloaded svg files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../context/themeContext';
+import { DarkThemeIcon } from './DarkThemeIcon';
 import { LightThemeIcon } from './LightThemeIcon';
 
 export const Navbar = () => {
     const { flipTheme, theme } = useContext(ThemeContext);
     return <nav className="navbar">
-        <h1 className="navbar-title" onClick={flipTheme}>Wordle</h1>
-        <div className="navbar-icon-container">
-            <LightThemeIcon />
+        <h1 className="navbar-title">Wordle</h1>
+        <div className="navbar-icon-container" onClick={flipTheme}>
+            {theme === "light" ? <LightThemeIcon /> : <DarkThemeIcon />}
         </div>
     </nav>
 };
